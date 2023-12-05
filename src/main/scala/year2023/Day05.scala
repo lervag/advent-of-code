b65b5ad58922651b419a4282efeb4e41e8abdd25
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-05 16:42:32 +0100

    feat: day 5 part 2 - brute force, bad

--- src/main/scala/year2023/Day05.scala
+++ src/main/scala/year2023/Day05.scala
@@ -1,43 +1,62 @@
 package year2023
 
 import scala.io.Source
+import scala.math.Ordering.Implicits._
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.immutable.NumericRange.Inclusive
+import scala.collection.immutable.NumericRange
 
 def day05: Unit = {
   val source = Source.fromFile("resources/2023/day-05")
   val lines = source.getLines.toVector
   source.close()
 
-  val seeds = lines(0).split(": ")(1).split(" ").toVector.map(BigInt(_))
-  val maps = mutable.Map.empty[
+  // Parse the input data
+  val mapBuilder = mutable.Map.empty[
     String,
     ArrayBuffer[(Inclusive[BigInt], Inclusive[BigInt])]
   ]
-
   var current: String = ""
   lines.drop(1).filterNot(_.size == 0).foreach(_ match {
     case s"$__new map:" =>
       current = __new
-      maps(current) = ArrayBuffer.empty[(Inclusive[BigInt], Inclusive[BigInt])]
+      mapBuilder(current) = ArrayBuffer.empty[(Inclusive[BigInt], Inclusive[BigInt])]
     case s"$_destination $_source $_length" =>
       val destination = BigInt(_destination)
       val source = BigInt(_source)
       val length = BigInt(_length)
-      maps(current) += ((
+      mapBuilder(current) += ((
         (source to source + length - 1),
         (destination to destination + length - 1),
         ))
   })
 
-  val part1 = seeds.map(seedToLocation(_, maps)).tapEach(println).min
+  val maps = mapBuilder.toMap.map { (name, array) => name -> array.toVector }
+  val seeds = lines(0).split(": ")(1).split(" ").toVector.map(BigInt(_))
+
+  // Solve Part 1
+  val part1 = seeds.map(seedToLocation(_, maps)).min
   println(s"Part 1: $part1")
+
+  // Solve Part 2
+  val seedRanges = seeds.grouped(2).toVector.map { pair =>
+    (pair(0) to pair(0) + pair(1) - 1)
+  }.sorted
+  println(s"There are ${seedRanges.size} ranges.")
+  val part2 = seedRanges.map { range =>
+    println(s"Starting on range: $range")
+    val x = range.tail.fold(seedToLocation(range.head, maps)) { (acc, next) =>
+      Ordering[BigInt].min(acc, seedToLocation(next, maps))
+    }
+    x
+  }.min
+  println(s"Part 2: $part2")
 }
 
 private def seedToLocation(
   input: BigInt,
-  maps: mutable.Map[String, ArrayBuffer[(Inclusive[BigInt], Inclusive[BigInt])]]): BigInt = {
+  maps: Map[String, Vector[(Inclusive[BigInt], Inclusive[BigInt])]]): BigInt = {
     var x: BigInt = input
 
     Array(
@@ -49,7 +68,7 @@ private def seedToLocation(
       "temperature-to-humidity",
       "humidity-to-location"
     ).foreach { map =>
-      x = followMap(x, map, maps)
+      x = followMap(x, maps(map))
     }
 
     x
@@ -57,9 +76,8 @@ private def seedToLocation(
 
 private def followMap(
   input: BigInt,
-  map: String,
-  maps: mutable.Map[String, ArrayBuffer[(Inclusive[BigInt], Inclusive[BigInt])]]): BigInt = {
-    maps(map).foreach { (sourceRange, destRange) =>
+  map: Vector[(Inclusive[BigInt], Inclusive[BigInt])]): BigInt = {
+    map.foreach { (sourceRange, destRange) =>
       if (sourceRange.contains(input))
         return destRange.start + input - sourceRange.start
     }
