4ef771626a2d922bb35ae00bd87d137ea0563910
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-05 15:24:40 +0100

    feat: day 5 part 1

--- src/main/scala/year2023/Day05.scala
+++ src/main/scala/year2023/Day05.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable.ArrayBuffer
 import scala.collection.immutable.NumericRange.Inclusive
 
 def day05: Unit = {
-  val source = Source.fromFile("resources/2023/day-05a")
+  val source = Source.fromFile("resources/2023/day-05")
   val lines = source.getLines.toVector
   source.close()
 
@@ -31,12 +31,28 @@ def day05: Unit = {
         ))
   })
 
-  seeds.map(seedToLocation(_, maps)).foreach(println)
+  val part1 = seeds.map(seedToLocation(_, maps)).tapEach(println).min
+  println(s"Part 1: $part1")
 }
 
 private def seedToLocation(
   input: BigInt,
   maps: mutable.Map[String, ArrayBuffer[(Inclusive[BigInt], Inclusive[BigInt])]]): BigInt = {
+    var x: BigInt = input
+
+    Array(
+      "seed-to-soil",
+      "soil-to-fertilizer",
+      "fertilizer-to-water",
+      "water-to-light",
+      "light-to-temperature",
+      "temperature-to-humidity",
+      "humidity-to-location"
+    ).foreach { map =>
+      x = followMap(x, map, maps)
+    }
+
+    x
 }
 
 private def followMap(
@@ -44,8 +60,8 @@ private def followMap(
   map: String,
   maps: mutable.Map[String, ArrayBuffer[(Inclusive[BigInt], Inclusive[BigInt])]]): BigInt = {
     maps(map).foreach { (sourceRange, destRange) =>
-      if (sourceRange.contains(seed))
-        return destRange.start + seed - sourceRange.start
+      if (sourceRange.contains(input))
+        return destRange.start + input - sourceRange.start
     }
     input
 }
