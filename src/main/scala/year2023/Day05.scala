2cbe5badc5882c01c98de3fe6716a3b6a4fb4f51
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-05 18:08:24 +0100

    feat: improve day 5 part 2

--- src/main/scala/year2023/Day05.scala
+++ src/main/scala/year2023/Day05.scala
@@ -15,24 +15,22 @@ def day05: Unit = {
   // Parse the input data
   val mapBuilder = mutable.Map.empty[
     String,
-    ArrayBuffer[(Inclusive[BigInt], Inclusive[BigInt])]
+    ArrayBuffer[(Inclusive[BigInt], BigInt)]
   ]
   var current: String = ""
   lines.drop(1).filterNot(_.size == 0).foreach(_ match {
     case s"$__new map:" =>
       current = __new
-      mapBuilder(current) = ArrayBuffer.empty[(Inclusive[BigInt], Inclusive[BigInt])]
+      mapBuilder(current) = ArrayBuffer.empty[(Inclusive[BigInt], BigInt)]
     case s"$_destination $_source $_length" =>
-      val destination = BigInt(_destination)
       val source = BigInt(_source)
-      val length = BigInt(_length)
       mapBuilder(current) += ((
-        (source to source + length - 1),
-        (destination to destination + length - 1),
+        (source to source + BigInt(_length) - 1),
+        BigInt(_destination) - source,
         ))
   })
 
-  val maps = mapBuilder.toMap.map { (name, array) => name -> array.toVector }
+  val maps = mapBuilder.toMap.map { (n, a) => n -> a.toVector.sorted }
   val seeds = lines(0).split(": ")(1).split(" ").toVector.map(BigInt(_))
 
   // Solve Part 1
@@ -43,7 +41,6 @@ def day05: Unit = {
   val seedRanges = seeds.grouped(2).toVector.map { pair =>
     (pair(0) to pair(0) + pair(1) - 1)
   }.sorted
-  println(s"There are ${seedRanges.size} ranges.")
   val part2 = seedRanges.map { range =>
     println(s"Starting on range: $range")
     val x = range.tail.fold(seedToLocation(range.head, maps)) { (acc, next) =>
@@ -56,9 +53,7 @@ def day05: Unit = {
 
 private def seedToLocation(
   input: BigInt,
-  maps: Map[String, Vector[(Inclusive[BigInt], Inclusive[BigInt])]]): BigInt = {
-    var x: BigInt = input
-
+  maps: Map[String, Vector[(Inclusive[BigInt], BigInt)]]): BigInt =
     Array(
       "seed-to-soil",
       "soil-to-fertilizer",
@@ -67,19 +62,12 @@ private def seedToLocation(
       "light-to-temperature",
       "temperature-to-humidity",
       "humidity-to-location"
-    ).foreach { map =>
-      x = followMap(x, maps(map))
-    }
-
-    x
-}
+    ).foldLeft(input) { (acc, map) => followMap(acc, maps(map)) }
 
 private def followMap(
   input: BigInt,
-  map: Vector[(Inclusive[BigInt], Inclusive[BigInt])]): BigInt = {
-    map.foreach { (sourceRange, destRange) =>
-      if (sourceRange.contains(input))
-        return destRange.start + input - sourceRange.start
-    }
-    input
-}
+  map: Vector[(Inclusive[BigInt], BigInt)]): BigInt =
+    map
+      .find { case (source, _) => source.contains(input) }
+      .map { case (source, diff) => input + diff }
+      .getOrElse(input)
