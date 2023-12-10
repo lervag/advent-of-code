6379a137cf0a3a375f5041829a56608de07d09b2
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-10 09:58:23 +0100

    chore: xxx

--- src/main/scala/year2023/Day10.scala
+++ src/main/scala/year2023/Day10.scala
@@ -8,4 +8,14 @@ def day10: Unit = {
   val lines = source.getLines.toVector
   source.close()
 
+  // lines -> board Vector(Vector(pipes))
+  // S -> board find S
+  // loop -> board, S find loop
+  //  find two pipes from S
+  //  follow one of the pipes back to S
+  //  store position and n steps
+  // loop: Vector(pos)
+
+  // How many steps along the loop does it take to get from the starting
+  // position to the point farthest from the starting position?
 }
