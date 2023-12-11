e75b04d0a23748726ced89c283afb81f39276199
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-11 22:13:18 +0100

    feat: solved day 11

--- src/main/scala/year2023/Day11.scala
+++ src/main/scala/year2023/Day11.scala
@@ -1,12 +1,36 @@
 package year2023
 
 import scala.io.Source
-import scala.collection.mutable.ArrayBuffer
-import scala.annotation.tailrec
 
 def day11: Unit = {
   val source = Source.fromFile("resources/2023/day-11")
-  val board = source.getLines.toVector.map(_.toVector)
+  val lines = source.getLines.toVector
   source.close()
 
+  val galaxies = lines.zipWithIndex.flatMap { (line, i) =>
+    line.zipWithIndex.collect { case ('#', j) => (i, j) }
+  }
+  val emptyRows = (0 until lines.size).toSet -- galaxies.map(_._1).toSet
+  val emptyCols = (0 until lines.size).toSet -- galaxies.map(_._2).toSet
+
+  def expandAndSum(n: Int): BigInt =
+    galaxies
+      .map { (i, j) =>
+        (i + n * emptyRows.count(_ < i), j + n * emptyCols.count(_ < j))
+      }
+      .combinations(2)
+      .map { points => distance(points(0), points(1)) }
+      .sum
+
+  val part1 = expandAndSum(1)
+  println(s"Part 1: $part1")
+
+  val part2 = expandAndSum(999999)
+  println(s"Part 2: $part2")
+}
+
+private def distance(p1: (Int, Int), p2: (Int, Int)): BigInt = {
+  val a = (p2._1 - p1._1).abs
+  val b = (p2._2 - p1._2).abs
+  BigInt(a) + BigInt(b)
 }
