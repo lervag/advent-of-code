949bdb894690f717ce040c4f01e56f2ba7392fae
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-12 16:05:51 +0100

    feat: prepare for day 12

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import year2023._
 
 @main def main: Unit = {
   val before = System.currentTimeMillis
-  day11
+  day12
   val after = System.currentTimeMillis
   println("Elapsed time: " + (after - before) + "ms")
 }
---
+++ src/main/scala/year2023/Day12.scala
@@ -0,0 +1,10 @@
+package year2023
+
+import scala.io.Source
+
+def day12: Unit = {
+  val source = Source.fromFile("resources/2023/day-12a")
+  val lines = source.getLines.toVector
+  source.close()
+
+}
