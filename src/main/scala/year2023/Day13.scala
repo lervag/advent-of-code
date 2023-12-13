edf95848a12db2c83aba2cf64dfddfb5b822c378
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-13 08:13:50 +0100

    feat: prepare day 13

---
+++ src/main/scala/year2023/Day13.scala
@@ -0,0 +1,14 @@
+package year2023
+
+import scala.io.Source
+import scala.collection.mutable
+
+def day13: Unit = {
+  val source = Source.fromFile("resources/2023/day-13a")
+  val lines = source.getLines.toVector
+  source.close()
+
+  // println(s"Part 1: $part1")
+
+  // println(s"Part 2: $part2")
+}
