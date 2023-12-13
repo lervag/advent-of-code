c9d21966381a5423fd8fcef7f8ac2407d9346277
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-13 08:23:32 +0100

    feat: add pseudo code

--- src/main/scala/year2023/Day13.scala
+++ src/main/scala/year2023/Day13.scala
@@ -8,7 +8,19 @@ def day13: Unit = {
   val lines = source.getLines.toVector
   source.close()
 
-  // println(s"Part 1: $part1")
+  // split lines into "chunks"
+  // split chunks into patterns
+  // parse pattern into tuple of
+  //  list of columns (BitMap)
+  //  list of rows (BitMap)
+  // for each list
+  //  for i from 1 to list length - 1
+  //    compare numbers in list (i-, i+)
+  //      if equal -- tthis is the reflection and i is my number
 
-  // println(s"Part 2: $part2")
+  val part1 = ??? // number of cols to the left + 100 number of rows above
+  println(s"Part 1: $part1")
+
+  val part2 = ???
+  println(s"Part 2: $part2")
 }
