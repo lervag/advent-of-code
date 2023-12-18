e4dbd74a16ef898e5ffaecc04479a7e7d2e4fc28
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-18 15:59:52 +0100

    feat: day 18 part 2

--- src/main/scala/year2023/Day18.scala
+++ src/main/scala/year2023/Day18.scala
@@ -1,20 +1,30 @@
 package year2023
 
 import scala.io.Source
-import scala.collection.mutable
 
 def day18: Unit = {
-  val source = Source.fromFile("resources/2023/day-18a")
-  val lines = source.getLines.toVector
+  val source = Source.fromFile("resources/2023/day-18")
+  val spec = source.getLines.toVector
+    .map { case s"$direction $length (#$rgb)" =>
+      (direction, length.toInt, rgb)
+    }
   source.close()
 
-  val spec = lines.map {
-    case s"$direction $length (#$rgb)" =>
-      (direction, length.toInt, rgb)
-  }
+  val corners1 = parseCorners(spec)
+  val part1 = area(corners1)
+  println(s"Part 1: $part1")
 
-  val corners = {
-    val cornersAux = spec.scanLeft((0, 0)) { case ((i, j), (dir, length, _)) =>
+  val corners2 = parseCorners(spec.map { (_, _, rgb) =>
+    val (dir, length) = parseColor(rgb)
+    (dir, length, rgb)
+  })
+  val part2 = area(corners2)
+  println(s"Part 2: $part2")
+}
+
+private def parseCorners(spec: Vector[(String, Int, String)]) = {
+  val cornersAux = spec
+    .scanLeft((0, 0)) { case ((i, j), (dir, length, _)) =>
       dir match {
         case "U" => (i - length, j)
         case "D" => (i + length, j)
@@ -22,16 +32,42 @@ def day18: Unit = {
         case "R" => (i, j + length)
       }
     }
-    val minI = cornersAux.map(_._1).min
-    val minJ = cornersAux.map(_._2).min
-    cornersAux.map { (i, j) => (i - minI, j - minJ) }
-  }
-
-  // I want area of polygon defined by given corners
-
-  // val part1 = "a"
-  // println(s"Part 1: $part1")
-
-  // val part2 = "a"
-  // println(s"Part 2: $part2")
+  val minI = cornersAux.map(_._1).min
+  val minJ = cornersAux.map(_._2).min
+  cornersAux
+    .map { (i, j) => (i - minI, j - minJ) }
+    .map { (i, j) => (BigInt(i), BigInt(j)) }
+    .reverse
 }
+
+private def parseColor(c: String) = {
+  val length = Integer.parseInt(c.take(5), 16)
+  val dir = c(5) match {
+    case '0' => "R"
+    case '1' => "D"
+    case '2' => "L"
+    case '3' => "U"
+  }
+  (dir, length)
+}
+
+private def area(corners: Vector[(BigInt, BigInt)]) = {
+  val area = areaShoelace(corners)
+  val b = circumference(corners)
+  innerPointsFromPicksTheorem(area, b) + b
+}
+
+private def circumference(points: Vector[(BigInt, BigInt)]) =
+  points
+    .zip(points.tail)
+    .map { case ((i0, j0), (i1, j1)) => (i0 - i1).abs + (j0 - j1).abs }
+    .sum
+
+private def areaShoelace(points: Vector[(BigInt, BigInt)]) =
+  points
+    .zip(points.tail)
+    .map { case ((i0, j0), (i1, j1)) => i0 * j1 - i1 * j0 }
+    .sum / 2
+
+private def innerPointsFromPicksTheorem(area: BigInt, b: BigInt) =
+  area - b / 2 + 1
