16edc0093990d4394f5b332f94902de37fbe4cbe
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-18 07:21:57 +0100

    feat: day 17 part 1

--- src/main/scala/year2023/Day17.scala
+++ src/main/scala/year2023/Day17.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.Stack
 import scala.collection.mutable.PriorityQueue
 
 def day17: Unit = {
-  val source = Source.fromFile("resources/2023/day-17a")
+  val source = Source.fromFile("resources/2023/day-17")
   val heatLoss = source.getLines.toVector
     .map { r => r.map(_.asDigit).toVector }
   source.close()
@@ -34,7 +34,7 @@ private def pathWithMinimalHeatLoss(
   val startState = State(start, 0, (0, 0), 0)
   val work = PriorityQueue[State](startState)
   val distances = mutable.Map
-    .empty[((Int, Int), (Int, Int)), Int]
+    .empty[((Int, Int), (Int, Int), Int), Int]
     .withDefaultValue(Integer.MAX_VALUE)
   val directions = Vector((-1, 0), (1, 0), (0, -1), (0, 1))
   val pathMap = mutable.Map.empty[State, State]
@@ -64,24 +64,26 @@ private def pathWithMinimalHeatLoss(
 
         val i = r + n
         val j = c + m
-        if (i >= 0 && i < heat.size && j >= 0 && j < heat.size && repeats < 3)
+        if (
+          i >= 0 && i < heat.size && j >= 0 && j < heat.size
+          // && repeats < 3
+          && repeats < 3
+        )
           Some(State((i, j), current.distance + heat(i)(j), (n, m), repeats))
         else
           None
       }
-      .filter { s => s.distance < distances((s.pos, s.direction)) }
+      .filter { s => s.distance < distances((s.pos, s.direction, s.repeats)) }
       .foreach { state =>
-        distances((state.pos, state.direction)) = state.distance
+        distances((state.pos, state.direction, state.repeats)) = state.distance
         pathMap(state) = current
         work.enqueue(state)
       }
   }
 
-  val endStates = pathMap.keys.filter(_.pos == destination)
-  println(endStates)
-  val endState = endStates.last
+  val endState = pathMap.keys.filter(_.pos == destination).minBy(_.distance)
 
-  pprintStates(getPath(endState), heat)
+  // pprintStates(getPath(endState), heat)
 
 
   getPath(endState)
