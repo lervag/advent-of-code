5e27212d4d304661dc11af3707467cdb330f23de
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-19 14:05:07 +0100

    doc: minor comments

--- src/main/scala/year2023/Day19.scala
+++ src/main/scala/year2023/Day19.scala
@@ -3,9 +3,11 @@ package year2023
 import scala.io.Source
 
 def day19: Unit = {
-  val source = Source.fromFile("resources/2023/day-19a")
+  // https://adventofcode.com/2023/day/19
+  val source = Source.fromFile("resources/2023/day-19")
   val lines = source.getLines().toVector
   source.close()
+  // 592 functions, 200 inputs
 
   // val part1 = ???
   // println(s"Part 1: $part1")
