868a18de0d9e00e32ec647bb7bfa68ac1b55cd04
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-22 12:04:07 +0100

    feat: day 22 prepared

---
+++ src/main/scala/year2023/Day22.scala
@@ -0,0 +1,15 @@
+package year2023
+
+import scala.io.Source
+import scala.collection.mutable
+
+def day22(): Unit = {
+  val source = Source.fromFile("resources/2023/day-22")
+  val lines = source.getLines().toVector
+  source.close()
+
+  val part1 = ???
+  println(s"Part 1: $part1")
+  // val part2 = ???
+  // println(s"Part 2: $part2")
+}
