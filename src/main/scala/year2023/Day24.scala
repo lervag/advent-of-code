6229f261a1fc41861db94e063927a72feacfccb6
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2023-12-26 13:15:01 +0100

    BAH

--- src/main/scala/year2023/Day24.scala
+++ src/main/scala/year2023/Day24.scala
@@ -4,7 +4,7 @@ import scala.io.Source
 import scala.collection.mutable
 
 def day24(): Unit = {
-  val source = Source.fromFile("resources/2023/day-24")
+  val source = Source.fromFile("resources/2023/day-24a")
   val hails =
     source.getLines().toVector.map { case s"$x, $y, $z @ $u, $v, $w" =>
       Hail(
@@ -25,8 +25,15 @@ def day24(): Unit = {
   // )
   // println(s"Part 1: $part1")
 
-  val part2 = newtonSolver(hails(0), hails(1), hails(2))
-  println(s"Part 2: $part2")
+  hails
+    .combinations(3)
+    .foreach { hh =>
+      val y = newtonSolver(hh(0), hh(1), hh(2))
+      println(y)
+    }
+    // .foreach(println)
+  // val part2 = newtonSolver(hails(0), hails(1), hails(2))
+  // println(s"Part 2: $part2")
 }
 
 sealed case class Hail(
@@ -68,7 +75,6 @@ private def intersection(h1: Hail, h2: Hail): Option[(Double, Double)] = {
 
 private def newtonSolver(h1: Hail, h2: Hail, h3: Hail) = {
   import breeze.linalg._
-  // Ax = b
   val matrixA = DenseMatrix(
     ((h1.v - h2.v), (h2.u - h1.u),     0L    , (h2.y - h1.y), (h1.x - h2.x),     0L    ),
     ((h1.v - h3.v), (h3.u - h1.u),     0L    , (h3.y - h1.y), (h1.x - h3.x),     0L    ),
@@ -78,20 +84,15 @@ private def newtonSolver(h1: Hail, h2: Hail, h3: Hail) = {
     ((h2.w - h3.w),     0L    , (h3.u - h2.u), (h3.z - h2.z),     0L    , (h2.x - h3.x))
   )
 
-  println(matrixA)
   val x = matrixA.mapValues { v => v.toDouble }
-  println(x)
-  println(rank(x))
-
-  h1
+  rank(x)
 }
 
 // Find stone (represented as a hail) H such that
 //
 //   t = collision(H, h) > 0 for all h in hails
 //
-// We need three known hails h. This gives 9 equations with 9 unknowns, that
-// we solve with an iterative method.
+// We need three known hails h. This gives 9 equations with 9 unknowns.
 //
 // Written out, the idea is to solve:
 //
@@ -108,26 +109,26 @@ private def newtonSolver(h1: Hail, h2: Hail, h3: Hail) = {
 // Start by removing t1, t2 and t3:
 //
 //   (y - y1)(u - u1) - (x - x1)(v - v1) = 0
-//   (z - z1)(u - u1) - (x - x1)(w - w1) = 0
 //   (y - y2)(u - u2) - (x - x2)(v - v2) = 0
-//   (z - z2)(u - u2) - (x - x2)(w - w2) = 0
 //   (y - y3)(u - u3) - (x - x3)(v - v3) = 0
+//   (z - z1)(u - u1) - (x - x1)(w - w1) = 0
+//   (z - z2)(u - u2) - (x - x2)(w - w2) = 0
 //   (z - z3)(u - u3) - (x - x3)(w - w3) = 0
 //
-// Write out and reorder the equations:
+// Or:
 //
-//   y u - y1 u - u1 y + u1 y1 - x v - x1 v1 + x1 v + v1 x = 0
-//   y u - y2 u - u2 y + u2 y2 - x v - x2 v2 + x2 v + v2 x = 0
-//   y u - y3 u - u3 y + u3 y3 - x v - x3 v3 + x3 v + v3 x = 0
-//   z u - z1 u - u1 z + u1 z1 - x w - x1 w1 + x1 w + w1 x = 0
-//   z u - z2 u - u2 z + u2 z2 - x w - x2 w2 + x2 w + w2 x = 0
-//   z u - z3 u - u3 z + u3 z3 - x w - x3 w3 + x3 w + w3 x = 0
+//   v1 x - u1 y - y1 u + x1 v + y u - x v + u1 y1 - x1 v1 = 0
+//   v2 x - u2 y - y2 u + x2 v + y u - x v + u2 y2 - x2 v2 = 0
+//   v3 x - u3 y - y3 u + x3 v + y u - x v + u3 y3 - x3 v3 = 0
+//   w1 x - u1 z - z1 u + x1 w + z u - x w + u1 z1 - x1 w1 = 0
+//   w2 x - u2 z - z2 u + x2 w + z u - x w + u2 z2 - x2 w2 = 0
+//   w3 x - u3 z - z3 u + x3 w + z u - x w + u3 z3 - x3 w3 = 0
 //
 // Subtract equations to make them linear:
 //
 //   (v1 - v2) x + (u2 - u1) y + (y2 - y1) u + (x1 - x2) v = x1 v1 - x2 v2 + u2 y2 - u1 y1
-//   (v1 - v3) x + (u3 - u1) y + (y3 - y1) u + (x1 - x3) v = x1 v1 - x3 v3 + u3 y3 - u1 y1
 //   (v2 - v3) x + (u3 - u2) y + (y3 - y2) u + (x2 - x3) v = x2 v2 - x3 v3 + u3 y3 - u2 y2
+//   (v3 - v1) x + (u1 - u3) y + (y1 - y3) u + (x3 - x1) v = x1 v1 - x3 v3 + u3 y3 - u1 y1
 //   (w1 - w2) x + (u2 - u1) z + (z2 - z1) u + (x1 - x2) w = x1 w1 - x2 w2 + u2 z2 - u1 z1
 //   (w1 - w3) x + (u3 - u1) z + (z3 - z1) u + (x1 - x3) w = x1 w1 - x3 w3 + u3 z3 - u1 z1
 //   (w2 - w3) x + (u3 - u2) z + (z3 - z2) u + (x2 - x3) w = x2 w2 - x3 w3 + u3 z3 - u2 z2
