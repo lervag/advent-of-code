fe7b8fa7d304919d5bd0836fd2b47b0cca1db2b2
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-12-04 21:05:29 +0100

    feat: day 4 part 2

--- src/main/scala/year2024/Day04.scala
+++ src/main/scala/year2024/Day04.scala
@@ -8,6 +8,33 @@ def day04: Unit = {
   val lines = source.getLines().toVector
   source.close()
 
+  println(part1(lines))
+  println(part2(lines))
+}
+
+def part2(lines: Vector[String]): Integer =
+  lines
+    .map(_.toVector)
+    .slide3x3 { (x, y) =>
+      if (x == "MAS" || x == "SAM") && (y == "MAS" || y == "SAM") then 1
+      else 0
+    }
+    .sum
+
+extension (vector: Vector[Vector[Char]])
+  def slide3x3[B](f: (String, String) => B): Vector[B] = {
+    (for {
+      i <- 0 to vector.length - 3
+      j <- 0 to vector.length - 3
+    } yield {
+      val d1 = (0 until 3).map(k => vector(i + k)(j + k)).mkString
+      val d2 = (0 until 3).map(k => vector(i + 2 - k)(j + k)).mkString
+
+      f(d1, d2)
+    }).toVector
+  }
+
+def part1(lines: Vector[String]): Integer = {
   val strings = {
     val transposed = lines.transpose.map(_.mkString)
     val diagonals1 = (for {
@@ -43,6 +70,6 @@ def day04: Unit = {
     P.string("XMAS").map(_ => 1).backtrack
       | P.anyChar.map(_ => 0)
   ).rep.map(_.foldLeft(0)(_ + _))
-  val counts = strings.map(parser.parseAll(_).getOrElse(0)).sum
-  println(counts)
+
+  strings.map(parser.parseAll(_).getOrElse(0)).sum
 }
