f92cfaee9a68eca5c76e4c1a2be68147e86f793b
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-12-10 18:34:57 +0100

    feat: day 10 part 2

--- src/main/scala/year2024/Day10.scala
+++ src/main/scala/year2024/Day10.scala
@@ -4,45 +4,51 @@ import scala.io.Source
 import scala.collection.mutable
 
 def day10: Unit = {
-  case class Map(heights: Vector[Vector[Int]]) {
+  type Point = (Int, Int)
+  extension (a: Point) def +(b: Point): Point = (a._1 + b._1, a._2 + b._2)
+
+  case class TopographicMap(heights: Vector[Vector[Int]]) {
     private val size = heights.size
 
-    def get(i: Int, j: Int): Int = {
+    def get(position: Point): Int = {
+      val (i, j) = position
       if i < 0 || j < 0 || i >= size || j >= size then -1
       else heights(j)(i)
     }
 
-    def get_possible_trailheads() = heights.zipWithIndex.flatMap { (row, j) =>
-      row.zipWithIndex.collect {
-        case (cell, i) if cell == 0 => (i, j)
+    def get_possible_trailheads(): Vector[Point] =
+      heights.zipWithIndex.flatMap { (row, j) =>
+        row.zipWithIndex.collect {
+          case (cell, i) if cell == 0 => (i, j)
+        }
       }
-    }
   }
 
   val source = Source.fromFile("resources/2024/day-10a")
-  val map = Map(source.getLines().toVector.map { _.toVector.map { _.asDigit } })
+  val topoMap = TopographicMap(
+    source.getLines().toVector.map { _.toVector.map { _.asDigit } }
+  )
   source.close()
 
-  val trailheads = map.get_possible_trailheads()
   val directions = List((1, 0), (0, -1), (-1, 0), (0, 1))
-  val cache = mutable.Map[(Int, Int), Set[(Int, Int)]]()
-  def reaches(height: Int, i: Int, j: Int): Set[(Int, Int)] = {
-    if cache.contains((i, j)) then cache((i, j))
-    else if height == 9 then Set((i, j))
-    else
+  val cache = mutable.Map[Point, List[(Point)]]()
+  def findTrailsDFS(height: Int, position: Point): List[(Point)] =
+    if height == 9 then List(position)
+    else cache.getOrElseUpdate(position, {
       val trails = directions
-        .map { (dx, dy) =>
-          val i1 = i + dx
-          val j1 = j + dy
-          (map.get(i1, j1), i1, j1)
+        .map { direction =>
+          val nextPos = position + direction
+          (topoMap.get(nextPos), nextPos)
         }
         .filter(_._1 == height + 1)
-      if trails.size == 0 then Set()
-      else
-        cache((i, j)) = trails.toSet.flatMap(reaches.tupled(_))
-        cache((i, j))
-  }
+      if trails.size == 0 then List()
+      else trails.toList.flatMap(findTrailsDFS.tupled(_))
+    })
 
-  val scores = trailheads.map { (i, j) => reaches(0, i, j).size }
-  println(scores.sum)
+  val trailheads = topoMap.get_possible_trailheads()
+  val scores = trailheads.map { pos => findTrailsDFS(0, pos) }
+  val part1 = scores.map(_.toSet.size).sum
+  val part2 = scores.map(_.size).sum
+  println(part1)
+  println(part2)
 }
