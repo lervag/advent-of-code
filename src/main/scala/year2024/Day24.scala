e18d95216f03907e9587606ff5f874fc19ed25b0
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-12-26 00:09:47 +0100

    feat: day 24 part 2

--- src/main/scala/year2024/Day24.scala
+++ src/main/scala/year2024/Day24.scala
@@ -2,7 +2,8 @@ package year2024
 
 import cats.parse.{Parser, Rfc5234}
 
-import scala.collection.mutable.Queue
+import scala.collection.mutable
+import scala.collection.mutable.{Queue, Stack}
 import scala.io.Source
 
 def day24: Unit = {
@@ -20,67 +21,103 @@ def day24: Unit = {
   val p_gate =
     (p_alnums
       ~ (
-        Parser.string(" AND ").as((a: Boolean, b: Boolean) => a && b)
-          | Parser.string(" OR ").as((a: Boolean, b: Boolean) => a || b)
-          | Parser.string(" XOR ").as((a: Boolean, b: Boolean) => a ^ b)
+        Parser.string(" AND ").as("and")
+          | Parser.string(" OR ").as("or")
+          | Parser.string(" XOR ").as("xor")
       )
       ~ p_alnums
       ~ (Parser.string(" -> ") *> p_alnums))
-      .map { case (((((in1, fn), in2), out))) =>
-        Gate(in1, in2, out, fn)
+      .map { case (((((in1, t), in2), out))) =>
+        Gate(in1, in2, out, t)
       }
   val parser =
     (p_value.repSep0(Rfc5234.lf) <* Rfc5234.lf.rep0) ~ (p_gate.repSep0(
       Rfc5234.lf
     ) <* Rfc5234.lf.rep0)
 
-  val gates = parser
+  parser
     .parseAll(input)
     .map { case (values, gates) =>
       Circuit.setSignals(values)
-      Circuit.processGates(gates)
-      gates
+      Circuit.setGates(gates)
+      Circuit.process()
     }
-    .getOrElse(Nil)
   val part1 = Circuit.getValue("z")
   println(part1)
 
-  // 4 par av gates har byttet output
-  // der er 222 gates og det er UMULIG å brute force her
+  // Del 2: Kretsen vil regne ut summen av x og y
+  //   Men: 4 par av gates har byttet output
+  //   Jeg skal finne hvilke 8 outputs som er byttet og gi svaret i sortert
+  //   rekkefølge
 
-  val z = Circuit.getValue("z")
-  val z0 = Circuit.getValue("x") + Circuit.getValue("y")
-  val r = z - z0
-  println(f"${z0.toBinaryString}%46s")
-  println(f"${z.toBinaryString}%46s")
-  println(f"${r.toBinaryString}%46s")
-  val m = r
-    .toBinaryString
-    .reverse
-    .zipWithIndex
-    .filter((c, i) => c == '1')
-    .map { (c, i) =>
-      f"z$i%02d"
-    }
-  println(m)
+  // Det jeg gjorde var å inspisere resultatet av kretsen sammenlignet med
+  // riktig beregnet x + y. Ved å variere x og y kunne jeg lokalisere
+  // z-punktene der det blir feil.
 
-  // gates
-  // -> tree z10 => (xs, ys)
+  // val x: Long = 0b111111111011111111100111111110011100111111111
+  // val y: Long = 0b000000001000000000100000000010000100000000001
+  // Circuit.setInput(x, y)
+  // Circuit.process()
+  // println(x + y)
+  // println(Circuit.getValue("z"))
 
-  // println(part2)
+  // Deretter tegnet jeg opp krets-trær fra disse og omliggende z-verdier. Ved
+  // å se på disse var det tydelig at noen av dem var byttet om. Jeg fant raskt
+  // ut at patternet for gates skal være ca slik: ^ -> | ^ -> & & -> ^ |
+
+  // Circuit.drawTree("z13")
+  // Circuit.drawTree("z14")
+  // Circuit.drawTree("z15")
+
+  // Da fant jeg altså følgende:
+  //
+  // z10 <- vrn & dkr
+  // kmb <- vrn ^ dkr
+  // z15 <- fsh | jkh
+  // tvp <- kvg ^ qts
+  // z25 <- y25 & x25
+  // dpg <- bpw ^ dgv
+  // mmf <- x35 & y35
+  // vdk <- x35 ^ y35
+  //
+  // Oppsamlet gir dette følgende svar:
+  println("dpg,kmb,mmf,tvp,vdk,z10,z15,z25")
 }
 
 private case class Gate(
     input1: String,
     input2: String,
     output: String,
-    gateFunc: (Boolean, Boolean) => Boolean
+    funcName: String
 ) {
-  override def toString(): String = s"Gate ($input1, $input2) -> $output"
+  val funcSymb = funcName match {
+    case "and" => "&"
+    case "or" => "|"
+    case "xor" => "^"
+  }
+  override def toString(): String = f"$output = $input1 $funcSymb $input2"
 }
 
 private object Circuit {
   private var memory: Map[String, Boolean] = Map()
+  private var gates: List[Gate] = Nil
+
+  def drawTree(node: String) = {
+    val stack = Stack[(Gate, Int)]()
+    stack ++= gates.filter(_.output == node).map((_, 0))
+    val visited = mutable.Set[Gate]()
+
+    var i = 0
+    while (stack.nonEmpty && i < 15) {
+      val (gate, depth) = stack.pop()
+      if !visited.contains(gate) then
+        visited += gate
+        println((" ").repeat(depth) + gate)
+        stack ++= gates.filter(_.output == gate.input1).map((_, depth + 2))
+        stack ++= gates.filter(_.output == gate.input2).map((_, depth + 2))
+      i += 1
+    }
+  }
 
   def getValue(name: "x" | "y" | "z") =
     memory.toVector
@@ -89,15 +126,13 @@ private object Circuit {
       .map(_._2)
       .foldRight(0.toLong)((b, i) => (i << 1) + (if (b) 1 else 0))
 
-  def processGates(gates: List[Gate]) = {
+  def process() = {
     val queue = Queue[Gate]() ++ gates
 
-    var i = 0
-    while (queue.nonEmpty && i < 10000) {
+    while (queue.nonEmpty) {
       val gate = queue.dequeue()
       if hasInputs(gate) then processGate(gate)
       else queue += gate
-      i += 1
     }
   }
 
@@ -105,10 +140,27 @@ private object Circuit {
     if hasInputs(gate) then
       val input1Value = memory(gate.input1)
       val input2Value = memory(gate.input2)
-      val result = gate.gateFunc(input1Value, input2Value)
+      val result = gate.funcName match {
+        case "and" => input1Value && input2Value
+        case "or" => input1Value || input2Value
+        case "xor" => input1Value ^ input2Value
+      }
       setSignal(gate.output, result)
       true
     else false
+
+  def setGates(gatesIn: List[Gate]): Unit = gates = gatesIn
+
+  def setInput(x: Long, y: Long) = {
+    println(y)
+    memory = Map()
+    val xbin = x.toBinaryString.reverse
+    val ybin = y.toBinaryString.reverse
+    (0 until 45) foreach { i =>
+      setSignal(f"x$i%02d", i < xbin.size && xbin(i) == '1')
+      setSignal(f"y$i%02d", i < ybin.size && ybin(i) == '1')
+    }
+  }
 
   def setSignals(signals: List[(String, Boolean)]): Unit =
     signals.foreach { (name, value) => setSignal(name, value) }
